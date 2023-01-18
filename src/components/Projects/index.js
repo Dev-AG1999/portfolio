99ae6ea102ab4efc0d666193b97ee5b3d0516097
Aishwariya Ghosal <aishwariya.ghosal04@gmail.com>
2023-01-18 18:57:55 +0530

    added projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -10,8 +10,13 @@ import form from "../../Assets/form.png";
 import "../Projects/style.css"
 import bagPencil from "../../Assets/briefcase.gif"
 import {Card} from "../cards"
+import bubblyChat from "../../Assets/bubblyChat.png"
+import ecomApp from "../../Assets/ecom-app.png"
 
 export const Projects = () => {
+
+
+  
   return (
     
       <div className="projects">
@@ -20,7 +25,7 @@ export const Projects = () => {
           <h1>PROJECTS</h1>
           <img alt="" src={bagPencil}/>
          </div>
-        <div className="project_wrapper" style={{display:"flex",height:"100%",width:"80%",margin:"auto",padding:"50px 0 0 0",flexDirection:"column"
+        <div className="project_wrapper" style={{display:"flex",height:"100%",width:"80%",margin:"auto",padding:"0",flexDirection:"column"
       }}>
          <div className="Heading">
          <h1 style={{color:"white",marginTop:"20px"}}>Projects</h1>
@@ -31,13 +36,15 @@ export const Projects = () => {
           <span style={{color:"#968b8b"}}>Please go to the following links to view all my projects in Github</span>
           <div className="project_container" style={{    display: "flex",justifyContent:"center",
     flexWrap: "wrap", margin:"20px 0"}}>
-      <Card  source="https://github.com/Dev-AG1999/instagram-clone" link="https://instagram-clonify.netlify.app/" image={instagramClone}/>
-      <Card source="https://github.com/Dev-AG1999/debouncing" link="https://musical-daifuku-ef3cb7.netlify.app/" image={debounce}/>
-      <Card source="https://github.com/Dev-AG1999/MY-TODO" link="https://peaceful-sprite-82da4f.netlify.app/" image={todo}/>
-      <Card source="https://github.com/Dev-AG1999/Creative-site-dark-theme-" link="https://majestic-sorbet-317212.netlify.app/" image={creative}/>
-      <Card source="https://github.com/Dev-AG1999/saath-care-test" link="https://saath-care-interview.netlify.app/" image={form}/>
-      <Card source="https://github.com/Dev-AG1999/ikken-light-theme" link="https://starlit-torte-8d0470.netlify.app/" image={ikken}/>
-      <Card source="https://github.com/Dev-AG1999/MyCart" link="https://practical-spence-030a31.netlify.app/" image={cart}/>
+            <Card abouts="Bubbly Chat - A real time chat application (reactJs,hooks,javascript,context,firebase)." source="https://github.com/Dev-AG1999/bubble-chat" link="https://bubble-chatapp.netlify.app/" image={bubblyChat}/>
+            <Card abouts="E-commerce website - A E-Commerce website with basic functionality like authentication,validation,product lists,category wise product search,product page,add to cart option with specific id of product to cart page and with remove cart option as well as pagination and while clicking add product for cart, incrementing the no of products in cart (reactJs,hooks,javascript,context,firebase,fetch)." source="https://github.com/Dev-AG1999/vetron__assesment" link="https://beamish-wisp-a3d9c0.netlify.app/" image={ecomApp}/>
+      <Card abouts="Instagram clone - Basic CRUD functionality using firebase database" source="https://github.com/Dev-AG1999/instagram-clone" link="https://instagram-clonify.netlify.app/" image={instagramClone}/>
+      <Card abouts="Implementation of Debounce and Throttle method"source="https://github.com/Dev-AG1999/debouncing" link="https://musical-daifuku-ef3cb7.netlify.app/" image={debounce}/>
+      <Card abouts="Todo-List - with full CRUD funtionality"source="https://github.com/Dev-AG1999/MY-TODO" link="https://peaceful-sprite-82da4f.netlify.app/" image={todo}/>
+      <Card abouts="A landing page in dark theme ,using HTML,CSS,JAVASCRIPT"source="https://github.com/Dev-AG1999/Creative-site-dark-theme-" link="https://majestic-sorbet-317212.netlify.app/" image={creative}/>
+      <Card abouts="Form - Using REACT.JS and TYPESCRIPT"source="https://github.com/Dev-AG1999/saath-care-test" link="https://saath-care-interview.netlify.app/" image={form}/>
+      <Card abouts="Random Website clone using HTML,CSS,JAVASCRIPT"source="https://github.com/Dev-AG1999/ikken-light-theme" link="https://starlit-torte-8d0470.netlify.app/" image={ikken}/>
+      <Card abouts="A Cart Page - using REACT.JS and TYPESCRIPT"source="https://github.com/Dev-AG1999/MyCart" link="https://practical-spence-030a31.netlify.app/" image={cart}/>
 
           </div>
          </div>
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,15 +1,34 @@
-
-export const Card=({image,link,source})=>{
-    return(
-      <>
-<a   target="_blank" rel="noreferrer" href={link} >
-<div className="card">
-        <img src={image} alt=""/>
-        <a  target="_blank" rel="noreferrer" href={source}><button style={{backgroundColor:"#e91e63",color:"white",padding:"5px",cursor:"pointer",width:"100%",border:"none"}}>Source Code</button></a>
-
-      </div>
-</a>
-
+export const Card = ({ image, link, source, abouts }) => {
+  return (
+    <>
+      <a
+        style={{ width: "350px" }}
+        target="_blank"
+        rel="noreferrer"
+        href={link}
+      >
+        <div className="card">
+          <img src={image} alt="" />
+          <div className="project_about">
+            <p className="normal_text">{abouts}</p>
+            <p className="hover_text">{abouts}</p>
+          </div>
+          <a target="_blank" rel="noreferrer" href={source}>
+            <button
+              style={{
+                backgroundColor: "#e91e63",
+                color: "white",
+                padding: "5px",
+                cursor: "pointer",
+                width: "100%",
+                border: "none",
+              }}
+            >
+              Source Code
+            </button>
+          </a>
+        </div>
+      </a>
     </>
-    )
-}
+  );
+};
